bfc2e71e33d3959db4bbd348a0038fd3811ec6a7
Vasya <inqire@gmail.com>
2018-03-18 01:11:00 +0300

    Added SteinAlgorithm and tests.

--- GCDAlgorithm/GCDSeeker.cs
+++ GCDAlgorithm/GCDSeeker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,22 +8,22 @@ using System.Threading.Tasks;
 namespace GCDAlgorithm
 {
     /// <summary>
-    /// Calculates greatest common divisor of numbers  by different methods
-    /// Euclidean and Stein's algorithms
+    /// Calculates greatest common divisor of numbers  by different methods.
+    /// Euclidean and Stein's algorithms.
     /// </summary>
     public static class GCDSeeker
     {
         /// <summary>
-        /// Finds the greatest common divisor of numbers by the Euclidean algorithm
+        /// Finds the greatest common divisor of numbers by the Euclidean algorithm.
         /// </summary>
-        ///  <param name="numbers">Numbers to find greatest common divisor</param> 
+        ///  <param name="numbers">Numbers to find greatest common divisor.</param> 
         /// <exception cref="ArgumentNullException">
-        /// Throws if numbers array is null
+        /// Throws if numbers array is null.
         /// </exception>
         /// <exception cref="ArgumentException">
-        /// Throws if length of an array of numbers  is smaller than 2
+        /// Throws if length of an array of numbers  is smaller than 2.
         /// </exception>
-        /// <returns> The greatest common divisor of numbers array </returns>
+        /// <returns> The greatest common divisor of numbers array. </returns>
         public static int EuclideanAlgorithm(params int[] numbers)
         {
             if (numbers == null)
@@ -50,16 +51,38 @@ namespace GCDAlgorithm
         }
 
         /// <summary>
-        /// Finds the greatest common divisor of numbers by the Stein's algorithm
+        /// Finds the greatest common divisor of numbers by the Euclidean algorithm.
         /// </summary>
-        ///  <param name="numbers">Numbers to find greatest common divisor</param> 
+        /// <param name="leadTime">Algorithm execution time.</param> 
+        /// <param name="numbers">Numbers to find greatest common divisor.</param> 
         /// <exception cref="ArgumentNullException">
-        /// Throws if numbers array is null
+        /// Throws if numbers array is null.
         /// </exception>
         /// <exception cref="ArgumentException">
-        /// Throws if length of an array of numbers  is smaller than 2
+        /// Throws if length of an array of numbers  is smaller than 2.
         /// </exception>
-        /// <returns> The greatest common divisor of numbers array </returns>
+        /// <returns> The greatest common divisor of numbers array. </returns>
+        public static int EuclideanAlgorithm(out TimeSpan leadTime, params int[] numbers)
+        {
+            var watcher = Stopwatch.StartNew();
+            var result = EuclideanAlgorithm(numbers);
+            watcher.Stop();
+            leadTime = watcher.Elapsed;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Finds the greatest common divisor of numbers by the Stein's algorithm.
+        /// </summary>
+        ///  <param name="numbers">Numbers to find greatest common divisor.</param> 
+        /// <exception cref="ArgumentNullException">
+        /// Throws if numbers array is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Throws if length of an array of numbers  is smaller than 2.
+        /// </exception>
+        /// <returns> The greatest common divisor of numbers array.</returns>
         public static int SteinAlgorithm(params int[] numbers)
         {
             if (numbers == null)
@@ -72,9 +95,41 @@ namespace GCDAlgorithm
                 throw new ArgumentException(nameof(numbers));
             }
 
+
             numbers = AbsArray(numbers);
 
-            return 0;
+            int resultGCD = numbers[numbers.Length - 1];
+            int i = numbers.Length - 2;
+            do
+            {
+                resultGCD = Stein(resultGCD, numbers[i]);
+                i--;
+            }
+            while (i > 0);
+
+            return resultGCD;
+        }
+
+        /// <summary>
+        /// Finds the greatest common divisor of numbers by the Stein's algorithm.
+        /// </summary>
+        /// <param name="leadTime">Algorithm execution time.</param> 
+        /// <param name="numbers">Numbers to find greatest common divisor.</param> 
+        /// <exception cref="ArgumentNullException">
+        /// Throws if numbers array is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Throws if length of an array of numbers  is smaller than 2.
+        /// </exception>
+        /// <returns> The greatest common divisor of numbers array.</returns>
+        public static int SteinAlgorithm(out TimeSpan leadTime, params int[] numbers)
+        {
+            var watcher = Stopwatch.StartNew();
+            var result = SteinAlgorithm(numbers);
+            watcher.Stop();
+            leadTime = watcher.Elapsed;
+
+            return result;
         }
 
         private static int Euclid(int value1, int value2)
@@ -83,7 +138,7 @@ namespace GCDAlgorithm
             {
                 return value2;
             }
-            
+
             if (value2.Equals(0))
             {
                 return value1;
@@ -101,6 +156,52 @@ namespace GCDAlgorithm
                 }
             }
             return value1;
+        }
+
+        private static int Stein(int value1, int value2)
+        {
+            if (value1.Equals(0))
+            {
+                return value2;
+            }
+
+            if (value2.Equals(0))
+            {
+                return value1;
+            }
+
+            int shitf;
+
+            for (shitf = 0; ((value1 | value2) & 1) == 0; shitf++)
+            {
+                value1 >>= 1;
+                value2 >>= 1;
+            }
+
+            while ((value1 & 1) == 0)
+            {
+                value1 >>= 1;
+            }
+
+            do
+            {
+                while ((value2 & 1) == 0)
+                {
+                    value2 >>= 1;
+                }
+
+                if (value1 > value2)
+                {
+                    var temp = value2;
+                    value2 = value1;
+                    value1 = temp;
+                }
+
+                value2 -= value1;
+            }
+            while (value2 != 0);
+
+            return value1 << shitf;
         }
 
         private static int[] AbsArray(int[] array)
--- GDCSeeker.Tests/GCDSeekerTests.cs
+++ GDCSeeker.Tests/GCDSeekerTests.cs
@@ -10,7 +10,7 @@ namespace GDCSeeker.Tests
         [TestCase(12, 24, 4, ExpectedResult = 4)]
         [TestCase(12, 24, 4, ExpectedResult = 4)]
         [TestCase(10, 20, ExpectedResult = 10)]
-        [TestCase(96, 288, ExpectedResult = 12)]
+        [TestCase(96, 288, ExpectedResult = 96)]
         [TestCase(0, 42, ExpectedResult = 42)]
         [TestCase(42, 0, ExpectedResult = 42)]
         [TestCase(0, 0, ExpectedResult = 0)]
@@ -23,6 +23,24 @@ namespace GDCSeeker.Tests
         public int EuclideanAlgorithmTests(params int[] numbers)
         {
             return GCDSeeker.EuclideanAlgorithm(numbers);
+        }
+
+        [TestCase(12, 24, 4, ExpectedResult = 4)]
+        [TestCase(12, 24, 4, ExpectedResult = 4)]
+        [TestCase(10, 20, ExpectedResult = 10)]
+        [TestCase(96, 288, ExpectedResult = 96)]
+        [TestCase(0, 42, ExpectedResult = 42)]
+        [TestCase(42, 0, ExpectedResult = 42)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(-1, 20, ExpectedResult = 1)]
+        [TestCase(-42, -84, ExpectedResult = 42)]
+        [TestCase(int.MaxValue, int.MaxValue, ExpectedResult = int.MaxValue)]
+        [TestCase(2, int.MaxValue, ExpectedResult = 1)]
+        [TestCase(int.MaxValue, 20, ExpectedResult = 1)]
+
+        public int SteinAlgorithmTests(params int[] numbers)
+        {
+            return GCDSeeker.SteinAlgorithm(numbers);
         }
     }
 }
